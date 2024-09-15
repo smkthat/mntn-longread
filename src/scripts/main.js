023b20b2c0d546caf7893e18a123ef750fc51e62
Mikhail Vlasenko <ya@smkthat.ru>
2024-09-16 10:21:48 +1100

    added `postcss-nested`, configure building

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,19 @@ import javascriptLogo from '../assets/javascript.svg';
 import viteLogo from '../assets/vite.svg';
 import { setupCounter } from './counter.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+document.querySelector('.app').innerHTML = `
+  <div class="app__container">
+    <a href="https://vitejs.dev" target="_blank" class="app__link">
+      <img src="${viteLogo}" class="app__logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${javascriptLogo}" class="app__logo app__logo--vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 class="app__title">Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button class="card__button" id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
+    <p class="app__read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import {defineConfig} from 'vite';
+import {patchCssModules} from 'vite-css-modules'
+import postcssNested from "postcss-nested";
 
 export default defineConfig({
     base: '/mntn-longread/',
     root: 'src',
+    plugins: [
+        patchCssModules()
+    ],
+    css: {
+        postcss: {
+            plugins: [postcssNested()],
+        }
+    },
     build: {
         outDir: '../dist',
         emptyOutDir: true,
+        target: 'esnext'
     },
 });
